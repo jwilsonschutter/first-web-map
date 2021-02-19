6761fd87f648b2b1f740d73b50fe8c82bc79980a
jwilsonschutter <jw6468@nyu.edu>
2021-02-18 23:39:46 -0500

    dsvfbgn
    does this work?

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFtZXN3aWxzb25zY2h1dHRlciIsImEiOiJja2xiamw0dTIwcjZlMm5xZXR1Z2oyNTZ0In0.ykjwA7KZIseTEFrKv-4aDw';
+
+var options = {
+  container: 'mapcountainer',
+  style: 'mapbox://styles/mapbox/dark-v10',
+  center: [-74.0060, 40.7128],
+  zoom: 10
+};
+
+var map = new mapboxgl.Map(options);
+
+var navigators = new mapboxgl.NavigationControl();
+map.addControl(navigators, 'bottom-right');
+
+//adding a marker
+
+var bigParky = [
+  {
+    name: 'PELHAM BAY PARK',
+    point: [-73.8104, 40.8677],
+    info: 'In the Last Quarter of 2020 this park had 152 Felonies'
+  },
+  {
+    name: 'VAN CORTLANDT PARK',
+    point: [-73.88825, 40.8972],
+    info: 'In the Last Quarter of 2020 this park had 15 Felonies'
+  },
+  {
+    name: 'ROCKAWAY BEACH AND BOARDWALK',
+    point: [-73.79084, 40.5888],
+    info: 'In the Last Quarter of 2020 this park had 7 Felonies'
+  },
+  {
+    name: 'FRESHKILLS PARK',
+    point: [-74.1880, 40.5772],
+    info: 'In the Last Quarter of 2020 this park had 6 Felonies'
+  },
+  {
+    name: 'FLUSHING MEADOWS CORONA PARK',
+    point: [-73.8428, 40.7400],
+    info: 'In the Last Quarter of 2020 this park had 5 Felonies'
+  }
+]
+
+
+bigParky.forEach(function(bigParky) {
+console.log(bigParky.name, bigParky.point, bigParky.info)
+
+new mapboxgl.Marker({
+color:"green",
+})
+  .setLngLat(bigParky.point)
+  .setPopup(new mapboxgl.Popup().setHTML(`
+    <div>
+      <h3 style="color: #4682b4;
+      font-family: sans-serif;
+      font-size: 20px;
+      text-align: center">${bigParky.name}</h3>
+      <p><b>${bigParky.info}</b></p>
+    </div>`))
+  .addTo(map);
+});
+
+document.getElementById('center').addEventListener('click', function() {
+  map.flyTo({
+    center: [-74.0060, 40.7128],
+    zoom: 10,
+    speed: 0.2,
+    curve: 1,
+    essential: true
+  })
+});
+  //set up dummy data
+// the sethmtl changes it to data.name it's pretty chill
+//forEach sets an array, so the first time the function runs it's equal to a certain object, the 2nd time it's the next, 3rd it's the next
+
+
+document.getElementById('toplegend').addEventListener('click', function() {
+  var remove = document.getElementById("onlegend");
+  if (remove.style.display === "none") {
+    remove.style.display = "block";
+  } else {
+    remove.style.display = "none";
+  }
+});
+
+/*shoutout to w3 schools for teaching me this https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp*/
